c4a3a657115249861f14c396ea2ceb00849ce4ee
Tusda <marcus.wiborg@outlook.com>
2024-09-16 15:54:35 +0200

    Initial commit

---
+++ Assets/Script/PlayerPrefs.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPrefs : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private TextMeshProUGUI text;
+
+    void Start()
+    {
+        inputField.onEndEdit.AddListener(EndEdit);
+
+        StartCoroutine(RoutineStringUpdate());
+    }
+
+    void EndEdit(string text)
+    {
+        SaveString();
+        print("Worked");
+    }
+    
+    // Put a string from inputfield and save the string to playerprefs
+    private void SaveString()
+    {
+        string word = inputField.text;
+        UnityEngine.PlayerPrefs.SetString("Word", word);
+    }
+    
+    // Get the string from playerprefs and show it on the screen
+    private void GetString()
+    {
+        string word = UnityEngine.PlayerPrefs.GetString("Word");
+        text.text = word;
+    }
+
+    private IEnumerator RoutineStringUpdate()
+    {
+        while (true)    
+        {
+            GetString();
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
