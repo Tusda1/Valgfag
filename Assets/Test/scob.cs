9ac6644b0ab5a791d95359eacd3561f95b4d5d98
Tusda <marcus.wiborg@outlook.com>
2024-09-23 19:53:29 +0200

    Guid

---
+++ Assets/Test/scob.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New scob", menuName = "scob")]
+public class scob : BaseScriptableObject
+{
+    public string scobName;
+    public int scobAge;
+}
